6958f866e19023fbf1178600ccf019d5c86417ee
steven nguyen <sqnguyen829@gmail.com>
2019-09-15 17:06:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,15 +1,16 @@
-def roll_call_dwarves(dwarves_list) # code an argument here
+def roll_call_dwarves(dwarves_list)
   # Your code here
   dwarves_list.each_with_index { |p, index| puts "#{index+1} #{p}" }
 end
 
-def summon_captain_planet (magic_words) # code an argument here
+def summon_captain_planet (magic_words) 
   # Your code here
   magic_words.map { |word| word.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(check_array)
   # Your code here
+  check_array.any? { |c| c.length > 4 }
 end
 
 def find_the_cheese# code an argument here
