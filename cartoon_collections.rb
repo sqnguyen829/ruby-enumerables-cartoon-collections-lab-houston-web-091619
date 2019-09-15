19616ab13fd865ffb2d8910276ded0b22e919b6d
steven nguyen <sqnguyen829@gmail.com>
2019-09-15 17:02:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,61 @@
+def roll_call_dwarves(dwarves_list) # code an argument here
+  # Your code here
+  dwarves_list.each_with_index { |p, index| puts "#{index+1} #{p}" }
+end
+
+def summon_captain_planet (magic_words) # code an argument here
+  # Your code here
+  magic_words.map { |word| word.capitalize + '!' }
+end
+
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 def roll_call_dwarves(array)
   array.each_with_index { |p, index| puts "#{index+1} #{p}" }
 end
@@ -19,3 +77,5 @@ def find_the_cheese(array)
     the_cheeses.include?(c)
   end
 end
+
+=end
