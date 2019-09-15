00bd6552b39eec777b8a1bfa4324d99276b06d51
steven nguyen <sqnguyen829@gmail.com>
2019-09-15 17:09:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,77 +10,16 @@ end
 
 def long_planeteer_calls(check_array)
   # Your code here
-  check_array.any? { |c| c.length > 4 }
+  check_array.any? { |words| words.length > 4 }
 end
 
 def find_the_cheese(food_list)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |c|
-    the_cheeses.include?(c)
+  food_list.find do |da_cheese|
+    the_cheeses.include?(da_cheese)
   end
   
 end
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
-def roll_call_dwarves(array)
-  array.each_with_index { |p, index| puts "#{index+1} #{p}" }
-end
-
-
-def summon_captain_planet(array)
-  array.map { |call| call.capitalize + '!' }
-end
-
-
-def long_planeteer_calls(array)
-  array.any? { |c| c.length > 4 }
-end
-
-
-def find_the_cheese(array)
-  the_cheeses = ["cheddar", "gouda", "camembert"]
-  array.find do |c|
-    the_cheeses.include?(c)
-  end
-end
-
-=end
