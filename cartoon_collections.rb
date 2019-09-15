9275e92a38d3c2d80189e55f1a0fda6878a40403
steven nguyen <sqnguyen829@gmail.com>
2019-09-15 17:10:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,3 +23,64 @@ def find_the_cheese(food_list)# code an argument here
 end
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
+def roll_call_dwarves(array)
+  array.each_with_index { |p, index| puts "#{index+1} #{p}" }
+end
+
+
+def summon_captain_planet(array)
+  array.map { |call| call.capitalize + '!' }
+end
+
+
+def long_planeteer_calls(array)
+  array.any? { |c| c.length > 4 }
+end
+
+
+def find_the_cheese(array)
+  the_cheeses = ["cheddar", "gouda", "camembert"]
+  array.find do |c|
+    the_cheeses.include?(c)
+  end
+end
+
+=end
